e9643e1b2d371a3a694f1002744570162a3e0284
Yair <rogues@es6.today>
2017-10-04 17:05:55 -0500

    update isOlderThan instructions

--- ex-02-isOlderThan.js
+++ ex-02-isOlderThan.js
@@ -2,7 +2,7 @@
  * isOlderThan()
  *
  * Write a method called .isOlderThan() that accepts a number argument
- * and returns a boolean value of `false` if the input number is less
+ * and returns a boolean value of `true` if the input number is less
  * than the value of the `.age` property
  *
 **/
@@ -33,6 +33,6 @@ console.assert (someGirl.isOlderThan(20) === false)
 // Instructor Evaluation
 // checking `isOlderThan` method with other context with .call method
 //   https://docs.microsoft.com/en-us/scripting/javascript/reference/call-method-function-javascript
-console.assert( someGirl.isOlderThan.call({ age: 20 }) === false)
-console.assert( someGirl.isOlderThan.call({ age: 30 }) === false)
-console.assert( someGirl.isOlderThan.call({ age: 15 }) === true)
+console.assert( someGirl.isOlderThan.call({ age: 20 }, 34) === false)
+console.assert( someGirl.isOlderThan.call({ age: 30 }, 32) === false)
+console.assert( someGirl.isOlderThan.call({ age: 15 }, 12) === true)
